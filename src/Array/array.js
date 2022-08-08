28476ca1bf47d21d2e884f1ac4bc73fa39c76676
Pooja-Vishwakarma <pooja85641@gmail.com>
2022-08-08 14:47:41 +0530

    Not imp

---
+++ src/Array/array.js
@@ -0,0 +1,12 @@
+//const months = [January,Febuary,March,April,May,June,July,August,September,October,November,Decemeber];
+
+//const result = _.chunk([months , 3]);
+//console.log(result);
+
+
+
+
+
+
+
+//module.exports.chunk = result;
