67d9bbb7eae8781c13ee8c6474b8097505b4f839
Pooja-Vishwakarma <pooja85641@gmail.com>
2022-08-15 12:46:49 +0530

    mongo-DB-0

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect("mongodb+srv://functionup-cohort:3bMssIUqmBmIqb3e@cluster0.3ccgp0v.mongodb.net/PK-100?retryWrites=true&w=majority"
+mongoose.connect("mongodb+srv://Plutonium-backend:Z5gM38tJVQ4U80eR@cluster0.shgml9u.mongodb.net/Pooja85641-DB"
 , {
    useNewUrlParser: true 
 }
