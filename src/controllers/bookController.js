6662de146228f2b6798764db41221bdbaa7d10b9
Pooja-Vishwakarma <pooja85641@gmail.com>
2022-08-16 14:36:54 +0530

    two more files

---
+++ src/controllers/bookController.js
@@ -0,0 +1,14 @@
+
+const createBook = async function(req,res){
+    let data = req.body;
+    let savedData = await UserModel.create(data);
+    res.send({msg: savedData})
+
+}
+
+const getAllBooks = async function(req,res){
+    let allBooks = await UserModel.find()
+    res.send({msg: allBooks})
+};
+module.exports.createBook = createBook;
+module.exports.getAllBooks = getAllBooks;
